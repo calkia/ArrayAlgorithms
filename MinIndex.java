22d6cb3d0acca8d4ae8fd4dff296de1c78920f1a
calkia <kcy.1288@gmail.com>
2026-01-21 12:24:55 -0500

    sdf

--- MinIndex.java
+++ MinIndex.java
@@ -1,6 +1,6 @@
 public class MinIndex {
     public static void main(String[] args) {
-        int [] nums = {-2,0,5,7,9};
+        int [] nums = {0,-2,5,7,9};
         int min = Integer.MAX_VALUE;
         int idx = 0;
         for(int i = 0; i<nums.length;i++){
