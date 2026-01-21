67319572e47b6eee8456f1dd3306bc1f98909799
calkia <kcy.1288@gmail.com>
2026-01-21 12:21:48 -0500

    asdfghjkl

---
+++ MinIndex.java
@@ -0,0 +1,14 @@
+public class MinIndex {
+    public static void main(String[] args) {
+        int [] nums = {-2,0,5,7,9};
+        int min = Integer.MAX_VALUE;
+        int idx = 0;
+        for(int i = 0; i<nums.length;i++){
+            if (nums[i]<min){
+                min = nums[i];
+                idx = i;
+            }
+        }
+        System.out.println(idx);
+    }
+}
